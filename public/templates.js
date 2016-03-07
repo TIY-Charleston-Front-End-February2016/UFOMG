950d7617ed966e83af3d78643d4dec453e21bd3b
bhare1987 <brandonlhare@gmail.com>
2016-03-06 21:54:32 -0500

    finished login, logout, addsighting, deletesighting, addUser

--- public/templates.js
+++ public/templates.js
@@ -1,10 +1,16 @@
 var templates = {
   feedItem: [
-    "<div class='feedItem' data-stellar-background-ratio='0.5'>",
+    "<div class='feedItem' data-id='<%= id %>' data-user='<%= userName %>'>",
       "<div class='feedInfoContainer'>",
-        "<span class='feedUser'><%= username %></span>",
+        "<span class='feedUser'><%= userName %></span>",
         "<span class='feedTime'><%= timestamp %></span>",
         "<span class='feedLocation'><%= location %></span>",
+        "<% if (obj.userName === sessionStorage.getItem('userName')) { %>",
+          "<div>",
+            "<span class='edit'>Edit</span>",
+            "<span class='delete'>X</span>",
+          "</div>",
+        "<% } %>",
       "</div>",
       "<span class='feedImg'><img src='<%= img %>'/></span>",
       "<p><%= text %></p>",
